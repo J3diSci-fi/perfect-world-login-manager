baee2ff0e77cf4c73e9f295c93b18517d6663eca
Jonas Nunes <jonas.nunes@ufrpe.br>
2024-06-18 16:35:12 -0300

    v1.0

--- main.cmd
+++
@@ -1,7 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
-set PATH=C:\Users\JONAS\AppData\Local\Programs\Python\PYTHON~1;%PATH%
-set PYTHONHOME=C:\Users\JONAS\AppData\Local\Programs\Python\Python312
-set NUITKA_PYTHONPATH=C:\Users\JONAS\Documents\perfect-world-login-manager;C:\Users\JONAS\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\JONAS\AppData\Local\Programs\Python\Python312\Lib;C:\Users\JONAS\AppData\Local\Programs\Python\Python312;C:\Users\JONAS\Documents\perfect-world-login-manager\env;C:\Users\JONAS\Documents\perfect-world-login-manager\env\Lib\site-packages;C:\Users\JONAS\Documents\perfect-world-login-manager\env\Lib\site-packages\win32;C:\Users\JONAS\Documents\perfect-world-login-manager\env\Lib\site-packages\win32\lib;C:\Users\JONAS\Documents\perfect-world-login-manager\env\Lib\site-packages\Pythonwin
-"%~dp0main.exe" %*
